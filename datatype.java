b68b6f050d4062f5e0e43081b92893ae9e23d23f
MEET <meetpansuriya93@gmail.com>
2025-06-15 00:47:15 +0530

    write a data type

---
+++ datatype.java
@@ -0,0 +1,12 @@
+public class datatype {
+    public static void main (String[] args) {
+        //primitive type data
+        int Rno = 186263;//int data type only small no store
+        char name = 'm';//only single character can store
+        long no = 38878648444L;//it can store long digit
+        float pi = 3.34F;//it store decimal number
+        byte srno = 64;// it store no between -128 to 127
+        boolean isreal = true;//it stores true and false
+
+    }
+}
