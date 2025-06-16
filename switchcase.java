cdb05e2334a3d03dbfdf4175ebd67049897e14ea
MEET <meetpansuriya93@gmail.com>
2025-06-16 18:37:59 +0530

    switch case

---
+++ switchcase.java
@@ -0,0 +1,31 @@
+public class switchcase {
+    public static void main(String[] args) {
+        int day = 1;
+        switch (day) {
+            case 1 :
+                System.out.println("monday");
+                break;
+            case 2 :
+                System.out.println("tuesday");
+                break;    
+            case 3 :
+                System.out.println("wednesday");
+                break;  
+            case 4 :
+                System.out.println("thurday");
+                break;
+            case 5 :
+                System.out.println("friday");
+                break;
+            case 6 :
+                System.out.println("saterday");
+                break;     
+            case 7 :
+                System.out.println("sunday");
+                break;  
+            default :
+                System.out.println("enter no. between 1 to 7");
+                break;                   
+        }
+    }
+}
