e80cb42256a01831e9f284089cad3e49b887247d
MEET <meetpansuriya93@gmail.com>
2025-06-16 22:50:45 +0530

    function done

---
+++ function.java
@@ -0,0 +1,10 @@
+public class function {
+    //make a function pass parameter
+    public static int sum(int a, int b){
+        System.out.println(a+b);
+        return 0;
+    }
+    public static void main(String[] args){
+        sum(10, 40);//call a sum function
+    }
+}
