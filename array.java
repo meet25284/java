45fdee83a989c7de312bbb16cde5a3ff825a7da7
MEET <meetpansuriya93@gmail.com>
2025-06-16 11:41:55 +0530

    arrays done

--- array.java
+++ array.java
@@ -1,12 +1,30 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class array {
     public static void main (String[] args) {
         //array can make using data type then squre bracket name of array
-        int[] marks = marks[4];
+        int[] marks = new int[3];
         marks[0]=70;
         marks[1]=34;
-        marks[2]=90
+        marks[2]=90;
 
         //it can print using its index
-        System.out.println(marks[1])
+        System.out.println(marks[1]);
+
+        //2d array 
+        int[][] finalmarks = {{63,34,34},{77,34,34}};
+
+        //access it using index
+        System.out.println(finalmarks[0][2]);
+
+        //lenth of array
+        System.out.println(finalmarks.length);
+
+        //asending order sort
+        Arrays.sort(marks);
+        System.out.println(marks[0]);
+
+        
     }
 }
