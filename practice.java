3d82f05bf1a845166099bdba926a187831a6e6b7
MEET <meetpansuriya93@gmail.com>
2025-06-24 21:45:33 +0530

    reversing string using recursion

--- practice.java
+++ practice.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
 public class practice {
-    public static void tower(int n, char src , char helper , char dest) {
-        if(n == 0){
-            return;
-        }
-        tower(n-1, src, dest, helper);  // Step 1
-        System.out.println("disk " + n + " move from " + src + " to " + dest);  // Step 2
-        tower(n-1, helper, src, dest);  // Step 3
+    public static void reverse(String str ,int n) {
+     if(n == 0){
+     System.out.print(str.charAt(n));
+        return;
+     }
+     System.out.print(str.charAt(n));
+     reverse(str ,n-1);
     }
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();  // Number of disks
-        tower(n, 'A', 'B', 'C');
+        String str = sc.nextLine();        
+        reverse(str ,(str.length())-1);
     }
 }
