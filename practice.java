89b10a76b2b09922ef9b5db51ece23365e7075d2
MEET <meetpansuriya93@gmail.com>
2025-06-24 21:10:29 +0530

    tower of hanoi

--- practice.java
+++ practice.java
@@ -1,20 +1,18 @@
 import java.util.*;
+
 public class practice {
-    
+    public static void tower(int n, char src , char helper , char dest) {
+        if(n == 0){
+            return;
+        }
+        tower(n-1, src, dest, helper);  // Step 1
+        System.out.println("disk " + n + " move from " + src + " to " + dest);  // Step 2
+        tower(n-1, helper, src, dest);  // Step 3
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String sb = sc.nextLine();
-        int n = sb.length();
-        StringBuilder reverse = new StringBuilder("");
-        for(int i = n-1;i >= 0; i--){
-            char revese = sb.charAt(i);
-            reverse.append(revese);
-        }
-        System.out.println(reverse);
-
-        
-
-
-
+        int n = sc.nextInt();  // Number of disks
+        tower(n, 'A', 'B', 'C');
     }
 }
