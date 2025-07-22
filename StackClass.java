82e1666b62428db07e80f8b020e6e6c748fd6daf
MEET <meetpansuriya93@gmail.com>
2025-07-22 09:49:31 +0530

    stack done

--- StackClass.java
+++ StackClass.java
@@ -1,5 +1,6 @@
+import java.util.*;
 public class StackClass {
-    //stack using linked list scratch code
+    // stack using linked list scratch code
     
     static class Node {
         int data;
@@ -41,13 +42,40 @@ public class StackClass {
             }
             return head.data;  
         }
+
+        //stack using Arraylist
+
+        static ArrayList<Integer> list = new ArrayList<Integer>();
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        public static void push(int data){
+            list.add(data);
+        }
+
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
     }
     public static void main(String[] args) {
         Stack s = new Stack();
         s.push(1);
-        s.push(1);
         s.push(2);
         s.push(3);
+        s.push(4);
 
         while(!s.isEmpty()) {
             System.out.println(s.peek());
